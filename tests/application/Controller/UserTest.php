59478a950a06f174eafba62eadde3cf815e61fa3
zyoncoder <15612116+zyoncoder@users.noreply.github.com>
2022-07-27 17:57:34 +0300

    tests

--- tests/application/Controller/UserTest.php
+++ tests/application/Controller/UserTest.php
@@ -2,14 +2,16 @@
 
 namespace App\Tests\Unit;
 
+use App\DataFixtures\FiveDeliveriesIn2Hours;
 use App\DataFixtures\FiveDeliveriesIn5Days;
 use App\DataFixtures\FiveRentalsIn5Days;
 use App\DataFixtures\FiveRideShareIn5Days;
+use App\DataFixtures\FiveRideShareIn8Hours;
+use App\DataFixtures\FiveRideSharesIn8Hours;
+use App\DataFixtures\ThreeRentals;
+use App\DataFixtures\SevenDeliveriesIn2Hours;
+use App\DataFixtures\ThreeRideShareIn4Hours;
 use App\DataFixtures\UserFixtures;
-use App\Entity\Delivery;
-use App\Entity\Rent;
-use App\Entity\Rideshare;
-use App\Entity\User;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use GuzzleHttp\Client;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
@@ -20,6 +22,7 @@ class UserTest extends WebTestCase
 
 protected  $databaseTool;
 
+    private Client $client;
 
     public function setUp(): void
     {
@@ -32,6 +35,8 @@ protected  $databaseTool;
         $this->databaseTool->loadFixtures([
             UserFixtures::class
         ]);
+
+        $this->client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
     }
 
     public function tearDown(): void
@@ -39,28 +44,25 @@ protected  $databaseTool;
         parent::tearDown();
 
         $this->truncateEntities();
-
     }
-    
+
+
     public function testApiReturns200HttpCode(): void
     {
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
+        $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=2022-01-01&to=2022-01-02');
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') . '/' . $now->format('Y-m-d H:i:s'));
 
         $this->assertEquals(200, $response->getStatusCode());
-
     }
 
     public function testApiResponseIsJson(): void
     {
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
+        $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=2022-01-01&to=2022-01-02');
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') . '/' . $now->format('Y-m-d H:i:s'));
 
         $this->assertIsArray(json_decode($response->getBody(), true));
 
@@ -69,12 +71,10 @@ protected  $databaseTool;
     public function testApiResponseHasBalanceKey(): void
     {
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
+        $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=&to=');
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') . '/' . $now->format('Y-m-d H:i:s'));
 
-        $this->assertEquals(200, $response->getStatusCode());
         $data = json_decode($response->getBody(true), true);
         $this->assertArrayHasKey('balance', $data);
     }
@@ -86,17 +86,13 @@ protected  $databaseTool;
             FiveDeliveriesIn5Days::class
         ]);
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
 
         $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=' . $now->format('Y-m-d H:i:s') .
-            '&to=' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
 
-        $this->assertEquals(200, $response->getStatusCode());
         $data = json_decode($response->getBody(true), true);
-        $this->assertArrayHasKey('balance', $data);
 
         $this->assertEquals(5, $data['balance']);
     }
@@ -108,17 +104,12 @@ protected  $databaseTool;
             FiveRentalsIn5Days::class
         ]);
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
-
         $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=' . $now->format('Y-m-d H:i:s') .
-            '&to=' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
 
-        $this->assertEquals(200, $response->getStatusCode());
         $data = json_decode($response->getBody(true), true);
-        $this->assertArrayHasKey('balance', $data);
 
         $this->assertEquals(5, $data['balance']);
     }
@@ -130,19 +121,147 @@ protected  $databaseTool;
             FiveRideShareIn5Days::class
         ]);
 
-        // create our http client (Guzzle)
-        $client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(5, $data['balance']);
+    }
+
+
+    public function testApiResponseUserBalanceValue5DeliveriesRidesharesAndRentalsIn5DifferentDays(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveDeliveriesIn5Days::class,
+            FiveRideShareIn5Days::class,
+            FiveRentalsIn5Days::class
+        ]);
 
         $now = new \DateTime();
 
-        $response = $client->get('/api/user/1/balance?from=' . $now->format('Y-m-d H:i:s') .
-            '&to=' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(15, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue5DeliveriesIn2Hours(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveDeliveriesIn2Hours::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 hours')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(10, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue5RidesharesIn8Hours(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveRideShareIn8Hours::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+8 hours')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(15, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue5DeliveriesIn2Hours5RidesharesIn8Hours(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveDeliveriesIn2Hours::class,
+            FiveRideShareIn8Hours::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+8 hours')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(25, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue5DeliveriesIn2Hours5RidesharesIn8Hours5RentalsIn5Days(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveDeliveriesIn2Hours::class,
+            FiveRideShareIn8Hours::class,
+            FiveRentalsIn5Days::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
 
         $this->assertEquals(200, $response->getStatusCode());
         $data = json_decode($response->getBody(true), true);
         $this->assertArrayHasKey('balance', $data);
 
-        $this->assertEquals(5, $data['balance']);
+        $this->assertEquals(30, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue5DeliveriesIn2Hours5RidesharesIn8Hours5DeliveriesIn5Days5Ridesharesin5Days5RentalsIn5Days(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            FiveDeliveriesIn5Days::class,
+            FiveRideShareIn5Days::class,
+            FiveDeliveriesIn2Hours::class,
+            FiveRideShareIn8Hours::class,
+            FiveRentalsIn5Days::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(40, $data['balance']);
+    }
+
+    public function testApiResponseUserBalanceValue7DeliveriesIn2Hours3RidesharesIn4Hours3Rentals(): void
+    {
+
+        $this->databaseTool->loadFixtures([
+            SevenDeliveriesIn2Hours::class,
+            ThreeRideShareIn4Hours::class,
+            ThreeRentals::class,
+        ]);
+
+        $now = new \DateTime();
+
+        $response = $this->client->get('/api/user/1/balance/' . $now->format('Y-m-d H:i:s') .
+            '/' . $now->modify('+5 days')->format('Y-m-d H:i:s'));
+
+        $data = json_decode($response->getBody(true), true);
+
+        $this->assertEquals(18, $data['balance']);
     }
 
     private function truncateEntities()

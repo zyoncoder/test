559450406bcbea9d09d26b8db4104f4067b42af4
zyoncoder <15612116+zyoncoder@users.noreply.github.com>
2022-07-27 17:56:59 +0300

    api validation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Service\UserService;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,19 +23,38 @@ class UserController extends AbstractController
 
     }
 
-    #[Route('/user/{id}/balance', name: 'app_user_get_balance', methods:["GET","POST"])]
-    public function getBalance(Request $request, ManagerRegistry $doctrine, int $id): JsonResponse
+    #[Route('/user/{id}/balance/{fromDate}/{toDate}', name: 'app_user_get_balance', methods:["GET","POST"])]
+    public function getBalance(Request $request, ManagerRegistry $doctrine, int $id, string $fromDate, string $toDate): JsonResponse
     {
+
+
+        $validator = Validation::createValidator();
+
+        if(!$validator->validate($id, new Assert\Positive())) {
+
+            return $this->json(['status' => 'error', 'message' => 'Id is not valid']);
+        }
+
+        if(!$validator->validate($fromDate, new Assert\DateTime())) {
+
+            return $this->json(['status' => 'error', 'message' => 'From date is not valid']);
+        }
+
+        if(!$validator->validate($toDate, new Assert\DateTime())) {
+
+            return $this->json(['status' => 'error', 'message' => 'To date is not valid']);
+        }
+
         $user = $doctrine->getRepository(User::class)->find($id);
 
         if (!$user) {
 
-            return $this->json(['message' => 'No user found'], 404);
+            return $this->json(['status' => 'error', 'message' => 'No user found'], 404);
         }
 
-        $balance = $this->userService->calculateCurrentBalance($user->getId(), $request->get('from'), $request->get('to'));
+        $balance = $this->userService->calculateCurrentBalance($user->getId(), $fromDate, $toDate);
 
-        return $this->json(['balance' => $balance]);
+        return $this->json(['status' => 'success', 'balance' => $balance]);
     }
 }
 
